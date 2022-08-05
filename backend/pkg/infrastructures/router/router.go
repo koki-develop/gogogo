719da0e740bb41cde26c936f52bf16e1493358ac
koki-develop <kou.pg.0131@gmail.com>
2022-08-06 08:50:01 +0900

    cors の設定を追加

--- backend/pkg/infrastructures/router/router.go
+++ backend/pkg/infrastructures/router/router.go
@@ -10,6 +10,14 @@ import (
 func New() *gin.Engine {
 	r := gin.Default()
 
+	r.Use(func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "*")
+		ctx.Header("Access-Control-Allow-Headers", "*")
+
+		ctx.Next()
+	})
+
 	r.GET("/h", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, map[string]string{"status": "ok"})
 	})

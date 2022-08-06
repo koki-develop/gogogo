f4acf05d324c521296440b641928005414870dcb
koki-develop <kou.pg.0131@gmail.com>
2022-08-06 14:56:39 +0900

    cats.json を返すようにした

--- backend/pkg/controllers/cats_controller.go
+++ backend/pkg/controllers/cats_controller.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/koki-develop/gogogo/backend/pkg/entities"
+	"github.com/koki-develop/gogogo/backend/pkg/infrastructures/s3"
 )
 
 type CatsController struct{}
@@ -14,17 +16,18 @@ func NewCatsController() *CatsController {
 }
 
 func (ctrl *CatsController) FindAll(ctx *gin.Context) {
-	cats := entities.Cats{
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
-		{URL: "https://koki.me/images/profile.png"},
+	s3cl := s3.New()
+
+	data, err := s3cl.Download("gogogo-cats", "cats.json")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, map[string]string{"message": "internal server error"})
+		return
+	}
+
+	var cats entities.Cats
+	if err := json.NewDecoder(data).Decode(&cats); err != nil {
+		ctx.JSON(http.StatusInternalServerError, map[string]string{"message": "internal server error"})
+		return
 	}
 
 	ctx.JSON(http.StatusOK, cats)

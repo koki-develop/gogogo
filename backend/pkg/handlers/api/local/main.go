ccc05ca2802daf97ba92f1eee697f45b457634f5
koki-develop <kou.pg.0131@gmail.com>
2022-08-18 07:39:40 +0900

    IS_LOCAL を設定する処理を削除

--- backend/pkg/handlers/api/local/main.go
+++ backend/pkg/handlers/api/local/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"os"
-
 	"github.com/koki-develop/gogogo/backend/pkg/infrastructures/router"
 )
 
 // ローカル実行用
 func main() {
-	os.Setenv("IS_LOCAL", "true")
-
 	r := router.New()
 	if err := r.Run(); err != nil {
 		panic(err)

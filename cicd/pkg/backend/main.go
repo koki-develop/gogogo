1fe1f3a5f58969415b2165e87b5d428d3a06cffa
koki-develop <kou.pg.0131@gmail.com>
2022-10-26 22:39:01 +0900

    backend をリファクタ

--- cicd/pkg/backend/main.go
+++ cicd/pkg/backend/main.go
@@ -12,29 +12,23 @@ type BuildOutput struct {
 }
 
 func Build(ctx context.Context, client *dagger.Client, src dagger.DirectoryID) (*BuildOutput, error) {
-	// initialize
-	cont := util.
-		NewContainer(client, src, "golang:1.19").
-		WithWorkdir("/app/backend")
-
-	// setup go-task
+	cont := newContainer(client, src)
 	cont = util.SetupTask(cont)
-
-	// build
 	cont = cont.Exec(dagger.ContainerExecOpts{Args: []string{"task", "build"}})
 
-	// get dist directory
 	d, err := cont.Directory("/app/backend/dist").ID(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	// run
 	if _, err := cont.ExitCode(ctx); err != nil {
 		return nil, err
 	}
+	return &BuildOutput{DistDirectoryID: d}, nil
+}
 
-	return &BuildOutput{
-		DistDirectoryID: d,
-	}, nil
+func newContainer(client *dagger.Client, src dagger.DirectoryID) *dagger.Container {
+	return util.
+		NewContainer(client, src, "golang:1.19").
+		WithWorkdir("/app/backend")
 }

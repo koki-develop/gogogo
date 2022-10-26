0b23558ee76bd6c06b1dfa859030bb301ec4c5d5
koki-develop <kou.pg.0131@gmail.com>
2022-10-26 22:23:15 +0900

    frontend.Deploy を実装

--- cicd/pkg/frontend/build.go
+++ cicd/pkg/frontend/build.go
@@ -28,3 +28,39 @@ func Build(ctx context.Context, client *dagger.Client, src dagger.DirectoryID) (
 
 	return &BuildOutput{}, nil
 }
+
+type DeployInput struct {
+	AwsAccessKeyIDSecretID     dagger.SecretID
+	AwsSecretAccessKeySecretID dagger.SecretID
+	AwsSessionTokenSecretID    dagger.SecretID
+}
+
+type DeployOutput struct{}
+
+func Deploy(ctx context.Context, client *dagger.Client, src dagger.DirectoryID, ipt *DeployInput) (*DeployOutput, error) {
+	// initialize
+	cont := util.
+		NewContainer(client, src, "golang:1.19").
+		WithWorkdir("/app/frontend")
+
+	// install unzip
+	cont = cont.
+		Exec(dagger.ContainerExecOpts{Args: []string{"apt", "update", "-qq"}}).
+		Exec(dagger.ContainerExecOpts{Args: []string{"apt", "install", "-y", "unzip"}})
+
+	// setup go-task
+	cont = util.SetupTask(cont)
+
+	// setup awscli
+	cont = util.SetupAWSCLI(cont)
+
+	// build
+	cont = cont.Exec(dagger.ContainerExecOpts{Args: []string{"task", "build"}})
+
+	// run
+	if _, err := cont.ExitCode(ctx); err != nil {
+		return nil, err
+	}
+
+	return &DeployOutput{}, nil
+}

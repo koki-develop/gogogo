22cc7fecf0168f271f829843e1a439ea678f970e
koki-develop <kou.pg.0131@gmail.com>
2022-10-26 20:46:26 +0900

    コメントを追記

--- cicd/pkg/frontend/build.go
+++ cicd/pkg/frontend/build.go
@@ -10,15 +10,19 @@ import (
 type BuildOutput struct{}
 
 func Build(ctx context.Context, client *dagger.Client, src dagger.DirectoryID) (*BuildOutput, error) {
+	// initialize
 	cont := util.NewContainer(client, src, "golang:1.19").
 		WithWorkdir("/app/frontend")
 
+	// setup go-task
 	cont = util.SetupTask(cont)
 
+	// build
 	cont = cont.Exec(dagger.ContainerExecOpts{
 		Args: []string{"task", "build"},
 	})
 
+	// run
 	if _, err := cont.ExitCode(ctx); err != nil {
 		return nil, err
 	}

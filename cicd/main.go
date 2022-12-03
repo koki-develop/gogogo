4ccb712bab034aa691714d6f427d7382defb80fc
koki-develop <kou.pg.0131@gmail.com>
2022-12-04 08:26:16 +0900

    infrastructure の build パイプラインを作成

--- cicd/main.go
+++ cicd/main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"dagger.io/dagger"
+	"github.com/koki-develop/gogogo/cicd/pkg/util"
 )
 
 func main() {
@@ -32,10 +33,10 @@ func main() {
 	defer client.Close()
 
 	// get secrets
-	// accessKeyID := client.Host().EnvVariable("AWS_ACCESS_KEY_ID").Secret()
-	// secretAccessKey := client.Host().EnvVariable("AWS_SECRET_ACCESS_KEY").Secret()
-	// sessionToken := client.Host().EnvVariable("AWS_SESSION_TOKEN").Secret()
-	// catApiKey := client.Host().EnvVariable("CAT_API_KEY").Secret()
+	accessKeyID := client.Host().EnvVariable("AWS_ACCESS_KEY_ID").Secret()
+	secretAccessKey := client.Host().EnvVariable("AWS_SECRET_ACCESS_KEY").Secret()
+	sessionToken := client.Host().EnvVariable("AWS_SESSION_TOKEN").Secret()
+	catApiKey := client.Host().EnvVariable("CAT_API_KEY").Secret()
 
 	// get src
 	root, err := filepath.Abs("..")
@@ -44,20 +45,56 @@ func main() {
 	}
 	src := client.Host().Directory(root)
 
-	// backend
+	// backend build
+	var bdist *dagger.Directory
 	{
 		// checkout
-		cont := client.Container().From("golang:1.19").
+		cont := client.Container().
+			From("golang:1.19").
 			WithMountedDirectory("/app", src).
 			WithWorkdir("/app/backend")
-		// setup environment variablees
-		cont = cont.WithEnvVariable("GOARCH", "amd64").
+		// setup environment variables
+		cont = cont.
+			WithEnvVariable("GOARCH", "amd64").
 			WithEnvVariable("GOOS", "linux")
 		// build
 		cont = cont.
 			WithExec([]string{"go", "build", "-ldflags", "-s -w", "-o", "dist/api", "./pkg/handlers/api/lambda"}).
 			WithExec([]string{"go", "build", "-ldflags", "-s -w", "-o", "dist/updatecats", "./pkg/handlers/updatecats"})
-		// run pipeline
+		bdist = cont.Directory("/app/backend/dist")
+		// exec pipeline
+		if _, err := cont.ExitCode(ctx); err != nil {
+			panic(err)
+		}
+	}
+
+	// infrastructure build
+	{
+		// checkout
+		cont := client.Container().
+			From("golang:1.19").
+			WithMountedDirectory("/app", src).
+			WithWorkdir("/app/infrastructure")
+		// setup environment variables and secrets
+		cont = cont.
+			WithEnvVariable("CI", "true").
+			WithSecretVariable("AWS_ACCESS_KEY_ID", accessKeyID).
+			WithSecretVariable("AWS_SECRET_ACCESS_KEY", secretAccessKey).
+			WithSecretVariable("AWS_SESSION_TOKEN", sessionToken).
+			WithSecretVariable("CAT_API_KEY", catApiKey)
+		// mount backend build
+		cont = cont.WithMountedDirectory("/app/backend/dist", bdist)
+		// install tools
+		cont = util.SetupUnzip(cont)
+		cont = util.SetupTerraform(cont, "1.2.3")
+		cont = util.SetupNodeJS(cont, "16.x")
+		// install dependencies
+		cont = cont.
+			WithExec([]string{"go", "get"}).
+			WithExec([]string{"yarn", "install", "--frozen-lockfile"})
+		// plan
+		cont = cont.WithExec([]string{"yarn", "run", "cdktf", "plan"})
+		// exec pipeline
 		if _, err := cont.ExitCode(ctx); err != nil {
 			panic(err)
 		}
